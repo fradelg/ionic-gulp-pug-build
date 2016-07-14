9e0b70c2ee831d1a0ef8e95f5c47db5088334b9a
Fco. Javier Delgado del Hoyo <frandelhoyo@gmail.com>
2016-07-14 11:21:52 +0200

    change .html to .pug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 
 module.exports = function(options) {
-  options.src = options.src || 'app/**/*.html';
+  options.src = options.src || 'app/**/*.pug';
   options.dest = options.dest || 'www/build';
   options.pug = options.pug || { pretty: true };
 
